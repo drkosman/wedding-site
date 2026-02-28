bf7b2292fde5fb67b197077810a571baf0a87ec1
kostamanser <kostamanser@gmail.com>
2026-02-28 12:44:29 +0000

    Deleted venue map and venue map 3d

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,16 +4,12 @@ import Schedule from '../components/Schedule';
 import Travel from '../components/Travel';
 import RSVPSection from '../components/RSVPSection';
 import Footer from '../components/Footer';
-// import VenueMap from "../components/VenueMap";
-import VenueMap3DScan from '../components/VenueMap3D/VenueMap3DScan';
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Details />
-      {/* <VenueMap /> */}
-      <VenueMap3DScan />
       <Schedule />
       <Travel />
       <RSVPSection />
